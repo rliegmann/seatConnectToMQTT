fbdfc37b89e598574374c8223bfcd46841347372
rliegmann <ralf.liegmann@web.de>
2023-03-25 15:15:32 +0100

    first push

---
+++ app.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python3
+import pprint
+import asyncio
+import logging
+import inspect
+import time
+import sys
+import os
+import json
+
+import paho.mqtt.client as mqtt
+broker_address= os.environ.get('MQTT_BROKER_SERVER')
+topic = os.environ.get('MQTT_BROKER_TOPIC')
+from aiohttp import ClientSession
+from datetime import datetime
+
+import schedule
+
+
+#import debugpy;
+#debugpy.listen(("0.0.0.0", 5678))
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+try:
+    from seatconnect import Connection
+except ModuleNotFoundError as e:
+    print(f"Unable to import library: {e}")
+    sys.exit(1)
+
+logging.basicConfig(level=logging.DEBUG)
+
+USERNAME = os.environ.get('SEAT_CONNECT_USER')
+PASSWORD = os.environ.get('SEAT_CONNECT_PASS')
+PRINTRESPONSE = True
+INTERVAL = int(os.environ.get('SETTINGS_INTERVAL'))
+
+COMPONENTS = {
+    'sensor': 'sensor',
+    'binary_sensor': 'binary_sensor',
+    'lock': 'lock',
+    'device_tracker': 'device_tracker',
+    'switch': 'switch',
+}
+
+RESOURCES = [
+		"adblue_level",
+		"auxiliary_climatisation",
+		"battery_level",
+		"charge_max_ampere",
+		"charger_action_status",
+		"charging",
+        "charge_rate",
+        "charging_power",
+		"charging_cable_connected",
+		"charging_cable_locked",
+		"charging_time_left",
+		"climater_action_status",
+		"climatisation_target_temperature",
+		"climatisation_without_external_power",
+		"combined_range",
+		"combustion_range",
+        "departure1",
+        "departure2",
+        "departure3",
+		"distance",
+		"door_closed_left_back",
+		"door_closed_left_front",
+		"door_closed_right_back",
+		"door_closed_right_front",
+		"door_locked",
+		"electric_climatisation",
+		"electric_range",
+		"energy_flow",
+		"external_power",
+		"fuel_level",
+		"hood_closed",
+		"last_connected",
+		"lock_action_status",
+		"oil_inspection",
+		"oil_inspection_distance",
+        "oildisplay_0",
+		"outside_temperature",
+		"parking_light",
+		"parking_time",
+		"pheater_heating",
+		"pheater_status",
+		"pheater_ventilation",
+		"position",
+		"refresh_action_status",
+		"refresh_data",
+        "request_flash",
+        "request_honkandflash",
+		"request_in_progress",
+		"request_results",
+		"requests_remaining",
+		"service_inspection",
+		"service_inspection_distance",
+		"sunroof_closed",
+		"trip_last_average_auxillary_consumption",
+		"trip_last_average_electric_consumption",
+		"trip_last_average_fuel_consumption",
+		"trip_last_average_speed",
+		"trip_last_duration",
+		"trip_last_entry",
+		"trip_last_length",
+		"trip_last_recuperation",
+		"trip_last_total_electric_consumption",
+		"trunk_closed",
+		"trunk_locked",
+		"vehicle_moving",
+		"window_closed_left_back",
+		"window_closed_left_front",
+		"window_closed_right_back",
+		"window_closed_right_front",
+		"window_heater",
+		"windows_closed",
+        "seat_heating"
+]
+
+def is_enabled(attr):
+    """Return true if the user has enabled the resource."""
+    #return attr in RESOURCES
+    return True
+
+async def runSeatConnect():
+    client =  mqtt.Client("P1") #Test
+    client.connect(broker_address) #Test
+
+    async with ClientSession(headers={'Connection': 'keep-alive'}) as session:
+        print('')
+        print('########################################')
+        print('#      Logging on to Seat Connect     #')
+        print('########################################')
+        print(f"Initiating new session to Seat Connect with {USERNAME} as username")
+        connection = Connection(session, USERNAME, PASSWORD, PRINTRESPONSE)
+        print("Attempting to login to the Seat Connect service")
+        print(datetime.now())
+        if await connection.doLogin():
+            print('Login success!')
+            print(datetime.now())
+            print('Fetching vehicles associated with account.')
+            await connection.get_vehicles()
+
+            instruments = set()
+            for vehicle in connection.vehicles:
+                txt = vehicle.vin
+                print('')
+                print('########################################')
+                print('#         Setting up dashboard         #')
+                print(txt.center(40, '#'))
+                dashboard = vehicle.dashboard(mutable=True)
+                print("------------------------------------------------------")
+                print(dashboard.instruments)
+                print("------------------------------------------------------")
+
+                for instrument in (
+                        instrument
+                        for instrument in dashboard.instruments
+                        if instrument.component in COMPONENTS
+                        or is_enabled(instrument.slug_attr)):
+
+                    instruments.add(instrument)
+            print('')
+            print('########################################')
+            print('#          Vehicles discovered         #')
+            print('########################################')
+            for vehicle in connection.vehicles:
+                print(f"\tVIN: {vehicle.vin}")
+                print(f"\tModel: {vehicle.model}")
+                print(f"\tManufactured: {vehicle.model_year}")
+                print(f"\tConnect service deactivated: {vehicle.deactivated}")
+                print("")
+                if vehicle.is_nickname_supported: print(f"\tNickname: {vehicle.nickname}")
+                #print(f"\tObject attributes, and methods:")
+                #for prop in dir(vehicle):
+                #    if not "__" in prop:
+                #        try:
+                #            func = f"vehicle.{prop}"
+                #            typ = type(eval(func))
+                #            print(f"\t\t{prop} - {typ}")
+                #        except:
+                #            pass
+
+        else:
+            return False
+
+        # Output all instruments and states
+        print('')
+        print('########################################')
+        print('#      Instruments from dashboard      #')
+        print('########################################')        
+        inst_list = sorted(instruments, key=lambda x: x.attr)
+        jsonToSend = {}
+        for instrument in inst_list:
+            print(f'{instrument.full_name} - {instrument.str_state} - attributes: {instrument.attributes}')
+            TROPIC = topic + "/single/" + format(instrument.attr)
+            PAYLOD = format(instrument.str_state)            
+            time.sleep(0.01)
+            client.publish(TROPIC,PAYLOD,0,True)
+            
+            #data = {}
+            #data[format(instrument.attr)] = instrument.str_state
+            #jsonToSend.append(data)
+            jsonToSend[format(instrument.attr)] = instrument.str_state
+            
+         
+        json_data = json.dumps(jsonToSend)           
+        client.publish(topic + "/json/",json_data,0,True)
+      
+async def main():
+    """Main method."""
+    while True:
+        await runSeatConnect()
+        await asyncio.sleep(INTERVAL)
+    
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
